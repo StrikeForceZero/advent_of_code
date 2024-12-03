0527732ddf4f5441ced841df252a5a4774c61710
bstriker <strikeforcezero@gmail.com>
2024-12-03 12:11:58 -0500

    feat: day 1 p1 and p2

---
+++ src/day1p1.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+
+pub trait SortCopied {
+    type Item;
+    fn sort_copied(&self) -> Vec<Self::Item>;
+}
+
+impl SortCopied for &[u32] {
+    type Item = u32;
+
+    fn sort_copied(&self) -> Vec<Self::Item> {
+        let mut a = self.to_vec();
+        a.sort();
+        a
+    }
+}
+
+fn calc_distance(a: &[u32], b: &[u32]) -> u32 {
+    let a = a.sort_copied();
+    let b = b.sort_copied();
+    let mut distance = 0;
+    for (a, b) in a.iter().zip(b.iter()) {
+        distance += a.abs_diff(*b);
+    }
+    distance
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lines = advent_of_code::read_input(1);
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+    for input_line in lines {
+        let input_line = input_line?;
+        let input_line = input_line
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse::<u32>().expect("Invalid input"))
+            .collect::<Vec<_>>();
+        a.push(input_line[0]);
+        b.push(input_line[1]);
+    }
+    let answer = calc_distance(&a, &b);
+    println!("Answer: {answer}");
+    Ok(())
+}
---
+++ src/day1p2.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+fn occurrence_map(items: &[u32]) -> HashMap<u32, u32> {
+    let mut map = HashMap::new();
+    for &item in items {
+        *map.entry(item).or_default() += 1;
+    }
+    map
+}
+
+fn calc_similarity_score(a: &[u32], b: &[u32]) -> u32 {
+    let a = occurrence_map(a);
+    let b = occurrence_map(b);
+    let mut similarity = 0;
+    for (&av, &ac) in a.iter() {
+        similarity += av * b.get(&av).cloned().unwrap_or_default() * ac;
+    }
+    similarity
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lines = advent_of_code::read_input(1);
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+    for input_line in lines {
+        let input_line = input_line?;
+        let input_line = input_line
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse::<u32>().expect("Invalid input"))
+            .collect::<Vec<_>>();
+        a.push(input_line[0]);
+        b.push(input_line[1]);
+    }
+    let answer = calc_similarity_score(&a, &b);
+    println!("Answer: {answer}");
+    Ok(())
+}
