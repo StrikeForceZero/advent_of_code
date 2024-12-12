04801973a97e3a419c582c13ce51ffa5e7516361
bstriker <strikeforcezero@gmail.com>
2024-12-12 14:25:23 -0500

    feat: add day 12 p1

--- src/bin/day12p1.rs
+++ src/bin/day12p1.rs
@@ -1,12 +1,17 @@
 use advent_of_code::utils::matrix::{MatrixDetails, MatrixIterator};
 use advent_of_code::utils::vec2::IntoIVec2;
 use glam::{IVec2, UVec2};
-use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::num::TryFromIntError;
 use thiserror::Error;
+use advent_of_code::read_input;
+use advent_of_code::utils::string::StringToCharsMatrix;
 
 fn main() -> anyhow::Result<()> {
+    let garden = read_input(12)?.to_chars_matrix();
+    let pas = calc_perimeter_areas(&garden);
+    let cost: usize = pas.iter().map(|pa| pa.perimeter_area.cost()).sum();
+    println!("answer: {cost}");
     Ok(())
 }
 
@@ -90,86 +95,93 @@ impl TryFrom<IVec2> for Direction {
     }
 }
 
-#[derive(Debug, Clone, Default, PartialEq, Eq)]
-struct CacheEntry<T> {
-    item: T,
-    surrounding: HashMap<Direction, Option<char>>,
+#[derive(Debug, Copy, Clone, Default, PartialEq, Ord, PartialOrd, Eq, Hash)]
+struct GardenBed {
+    label: char,
+    perimeter_area: PerimeterArea,
 }
 
-impl<T> CacheEntry<T>
-where
-    T: Default,
-{
-    fn new(item: T) -> Self {
-        Self {
-            item,
-            ..Default::default()
-        }
-    }
-}
-
-fn calc_perimeter_areas(garden: &Vec<Vec<char>>) -> HashMap<char, PerimeterArea> {
+fn calc_perimeter_areas(garden: &Vec<Vec<char>>) -> Vec<GardenBed> {
     let matrix_details = MatrixDetails::from_matrix(garden);
-    let mut perimeter_areas = HashMap::<char, PerimeterArea>::new();
-    let mut cache = HashMap::<UVec2, CacheEntry<char>>::new();
-    for cur in MatrixIterator::new(garden) {
-        let (cur_pos, cur_char) = cur;
-        cache
-            .entry(cur_pos)
-            .or_insert_with(|| CacheEntry::new(*cur_char));
-        for direction in Direction::all() {
-            let Some(cache_entry) = cache.get(&cur_pos) else { unreachable!() };
-            if cache_entry.surrounding.contains_key(&direction) {
-                continue;
-            }
 
-            let next: Option<(UVec2, &char)> = direction.next(cur_pos).ok().and_then(|next_pos| {
-                // get next char
+    let mut results = vec![];
+    let mut seen = HashSet::new();
+    'matrix: for cur in MatrixIterator::new(garden) {
+        let (cur_pos, &cur_char) = cur;
+
+        if seen.contains(&cur_pos) {
+            continue 'matrix;
+        }
+
+        let mut pa = PerimeterArea::default();
+
+        let next_pos = |pos: UVec2, direction: Direction| -> Option<UVec2> {
+            direction.next(pos).ok().and_then(|next_pos| {
                 if matrix_details.is_within_bounds(next_pos) {
-                    let next_char = &garden[next_pos.y as usize][next_pos.x as usize];
-
-                    // add inverse
-                    let inverse_cache_entry = cache.entry(next_pos).or_insert_with(|| CacheEntry::new(*next_char));
-                    inverse_cache_entry
-                        .surrounding
-                        .entry(direction.inverse())
-                        .or_insert(Some(*cur_char));
-
-                    Some((next_pos, next_char))
-                } else {
-                    None
+                    let next_char = garden[next_pos.y as usize][next_pos.x as usize];
+                    if next_char == cur_char {
+                        return Some(next_pos);
+                    }
                 }
-            });
+                None
+            })
+        };
 
-            let next_char = next.map(|(_, next_char)| *next_char);
-            let Some(cache_entry) = cache.get_mut(&cur_pos) else {
-                unreachable!()
-            };
-            cache_entry.surrounding.insert(direction, next_char);
-        }
-    }
-    for (_pos, cache_entry) in cache {
-        let pa = perimeter_areas.entry(cache_entry.item).or_default();
-        pa.area += 1;
-        for (_, next_char) in cache_entry.surrounding {
-            if Some(cache_entry.item) != next_char {
-                pa.perimeter += 1;
+        let mut search_queue = VecDeque::from([cur_pos]);
+        'search: while let Some(cur_pos) = search_queue.pop_front() {
+            if seen.contains(&cur_pos) {
+                continue 'search;
+            }
+            seen.insert(cur_pos);
+
+            pa.area += 1;
+
+            'direction: for direction in Direction::all() {
+                let Some(next_pos) = next_pos(cur_pos, direction) else {
+                    pa.perimeter += 1;
+                    continue 'direction;
+                };
+                if seen.contains(&next_pos) {
+                    continue 'direction;
+                }
+                search_queue.push_front(next_pos);
             }
         }
+
+        results.push(GardenBed {
+            label: cur_char,
+            perimeter_area: pa,
+        });
+        seen.insert(cur_pos);
     }
-    perimeter_areas
+    results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use advent_of_code::utils::string::{deformat_string, StringToCharsMatrix};
-    use std::collections::HashSet;
+    use itertools::Itertools;
+
     #[test]
     fn calc_perimeter_areas_test() {
-        let matrix = vec![vec!['A';2];2];
+        let matrix = deformat_string(
+            "
+                AA
+                AA
+            ",
+        ).to_chars_matrix();
         let pas = calc_perimeter_areas(&matrix);
-        assert_eq!(pas, HashMap::from([('A', PerimeterArea { area: 4, perimeter: 8 })]));
+        assert_eq!(
+            pas,
+            vec![GardenBed {
+                label: 'A',
+                perimeter_area: PerimeterArea {
+                    area: 4,
+                    perimeter: 8
+                },
+            },],
+        );
 
         let matrix = deformat_string(
             "
@@ -177,12 +189,27 @@ mod tests {
                 AA
             ",
         )
-            .to_chars_matrix();
+        .to_chars_matrix();
         let pas = calc_perimeter_areas(&matrix);
-        assert_eq!(pas, HashMap::from([
-            ('A', PerimeterArea { area: 3, perimeter: 8 }),
-            ('B', PerimeterArea { area: 1, perimeter: 4 }),
-        ]))
+        assert_eq!(
+            pas,
+            vec![
+                GardenBed {
+                    label: 'A',
+                    perimeter_area: PerimeterArea {
+                        area: 3,
+                        perimeter: 8
+                    },
+                },
+                GardenBed {
+                    label: 'B',
+                    perimeter_area: PerimeterArea {
+                        area: 1,
+                        perimeter: 4
+                    },
+                },
+            ],
+        )
     }
     #[test]
     fn examples() -> anyhow::Result<()> {
@@ -201,102 +228,96 @@ mod tests {
             ",
         )
         .to_chars_matrix();
-        let perimeter_areas = calc_perimeter_areas(&garden);
-        let expected = HashMap::from([
-            (
-                'R',
-                PerimeterArea {
+        let perimeter_areas = calc_perimeter_areas(&garden)
+            .into_iter()
+            .sorted()
+            .collect::<Vec<_>>();
+        let expected = vec![
+            GardenBed {
+                label: 'R',
+                perimeter_area: PerimeterArea {
                     area: 12,
                     perimeter: 18,
                 },
-            ), // 216
-            (
-                'I',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'I',
+                perimeter_area: PerimeterArea {
                     area: 4,
                     perimeter: 8,
                 },
-            ), // 32
-            (
-                'C',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'C',
+                perimeter_area: PerimeterArea {
                     area: 14,
                     perimeter: 28,
                 },
-            ), // 392
-            (
-                'F',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'F',
+                perimeter_area: PerimeterArea {
                     area: 10,
                     perimeter: 18,
                 },
-            ), // 180
-            (
-                'V',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'V',
+                perimeter_area: PerimeterArea {
                     area: 13,
                     perimeter: 20,
                 },
-            ), // 260
-            (
-                'J',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'J',
+                perimeter_area: PerimeterArea {
                     area: 11,
                     perimeter: 20,
                 },
-            ), // 220
-            (
-                'C',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'C',
+                perimeter_area: PerimeterArea {
                     area: 1,
                     perimeter: 4,
                 },
-            ), // 4
-            (
-                'E',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'E',
+                perimeter_area: PerimeterArea {
                     area: 13,
                     perimeter: 18,
                 },
-            ), // 234
-            (
-                'I',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'I',
+                perimeter_area: PerimeterArea {
                     area: 14,
                     perimeter: 22,
                 },
-            ), // 308
-            (
-                'M',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'M',
+                perimeter_area: PerimeterArea {
                     area: 5,
                     perimeter: 12,
                 },
-            ), // 60
-            (
-                'S',
-                PerimeterArea {
+            },
+            GardenBed {
+                label: 'S',
+                perimeter_area: PerimeterArea {
                     area: 3,
                     perimeter: 8,
                 },
-            ), // 24
-        ]);
+            },
+        ]
+        .into_iter()
+        .sorted()
+        .collect::<Vec<_>>();
         assert_eq!(
-            perimeter_areas.len(), expected.len(),
-            "incorrect number of PerimeterAreas"
+            perimeter_areas,
+            expected,
         );
-        assert_eq!(
-            perimeter_areas.keys().collect::<HashSet<_>>(),
-            expected.keys().collect::<HashSet<_>>(),
-            "incorrect PerimeterArea keys"
-        );
-        for (char, perimeter_area) in perimeter_areas {
-            println!("{char}: {}", perimeter_area.cost());
-            assert_eq!(
-                perimeter_area, expected[&char],
-                "incorrect PerimeterArea for {char}"
-            );
-        }
         Ok(())
     }
 }
